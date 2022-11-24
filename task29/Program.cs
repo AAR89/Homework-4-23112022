84138530806f29c46278da79d15edc4bf8b3ff21
Alex Ur <ru.booking@yandex.ru>
2022-11-24 15:35:56 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите размер массива: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[Length];
+
+void FillArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(0, 101);
+
+        if (i == Array.Length - 1)
+        {
+            Array[i] = new Random().Next(0, 101);
+            Console.Write('[' + string.Join(",", Array) + ']');
+        }
+    }
+    return;
+}
+FillArray(Array);
